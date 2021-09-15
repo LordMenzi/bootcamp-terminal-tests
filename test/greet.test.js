26a521862bc432dc9d7f4825fa9851fd4fd4b6d5
Menzi Mkhombo <tukulloyd@gmail.com>
2021-09-15 22:54:23 +0200

    Added changes greet expectations

--- test/greet.test.js
+++ test/greet.test.js
@@ -3,7 +3,7 @@ let greet=require("../greet");
 
 describe("the greet function" , function(){
     it('should greet Andrew correctly', function(){
-        assert.equal('HELLO, ANDREW', greet('Andrew'));
+        assert.equal('HELLO, Menzi', greet('Andrew'));
     });
     it('should greet Karen correctly', function(){
         // this test will fail - can you fix it?
