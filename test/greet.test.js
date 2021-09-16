628c6123c66e1e46318c6147f86f8103a055f839
Menzi Mkhombo <tukulloyd@gmail.com>
2021-09-16 11:14:43 +0200

    Added changes greet Ann

--- test/greet.test.js
+++ test/greet.test.js
@@ -3,11 +3,10 @@ let greet=require("../greet");
 
 describe("the greet function" , function(){
     it('should greet Andrew correctly', function(){
-        assert.equal('HELLO, ANDREW', greet('Andrew'));
+        assert.equal('HELLO, Ann', greet('Andrew'));
     });
     it('should greet Karen correctly', function(){
         // this test will fail - can you fix it?
         assert.equal('HELLO, KAREN', greet('Karen'));
     });
 });
- 
